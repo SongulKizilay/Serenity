d48c92f7ddf7be342450dd57fdfbbc71c6fd7870
Songül Kızılay <44816982+SongulKizilay@users.noreply.github.com>
2023-08-21 12:54:37 +0300

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt install golang-go 
+sudo apt  install  gcc-go
+python -m pip install colorama
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
